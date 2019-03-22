9ef049a2e57160cd1521578d2014534191c8d2aa
zackn9ne <zackn9ne@gmail.com>
2019-03-22 13:32:15 -0400

    Update chrome_exten_interactive.sh

--- chrome_exten_interactive.sh
+++ chrome_exten_interactive.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+open /Applications/Google\ Chrome.app/
+sleep 5
 $(osascript <<EOD
-display alert "This is an alert" buttons {"No", "Yes"}
+display alert "Did chrome load?" buttons {"No", "Yes"}
 if button returned of result = "No" then
      display alert "No was clicked"
 else
     if button returned of result = "Yes" then
-         do shell script "pwd"
+        do shell script "open -a 'Google Chrome' https://chrome.google.com/webstore/detail/virtru-email-protection-f/nemmanchfojaehgkbgcfmdiidbopakpp?hl=en-US"
+
     end if
 end if
 EOD)
