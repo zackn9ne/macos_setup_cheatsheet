52124a44b75fdb9db06b2d791ff0d1ed760a3a25
zackn9ne <zackn9ne@gmail.com>
2019-05-14 11:37:38 -0400

    Update host-name-changer.sh

--- host-name-changer.sh
+++ host-name-changer.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "wElCome ToO Zackn9neZ HostName ChanG3r"
 read -p "Enter Your Hostname eg. NY-iMac-SomeBody: "  HWNAME
 sudo scutil --set HostName "$HWNAME"
 sudo scutil --set ComputerName "$HWNAME"
