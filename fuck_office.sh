091876344658fef7f94d440a30a4cb7d91708adf
zackn9ne <zackn9ne@gmail.com>
2019-06-20 12:11:18 -0400

    Update fuck_office.sh

--- fuck_office.sh
+++ fuck_office.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rm -rf "/Applications/Microsoft;rm -rf /Applications/Microsoft;rm -rf /Applications/Microrm -rf "/Applications/Microsoft Word";rm -rf "/Applications/Microsoft Excel";rm -rf "/Applications/Microsoft PowerPoint";rm -rf "/Applications/Microsoft Outlook" ;soft;rm -rf /Applications/Microsoft;
+rm -rf "/Applications/Microsoft Word.app";rm -rf "/Applications/Microsoft Excel.app";rm -rf "/Applications/Microsoft PowerPoint.app";rm -rf "/Applications/Microsoft Outlook.app" ;
