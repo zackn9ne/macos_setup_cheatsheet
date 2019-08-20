c91926daf3b07ca2b6df678be348c06b450ce4a2
zackn9ne <zackn9ne@gmail.com>
2019-08-20 15:11:33 -0400

    Update lpadmin_minus_p_printerscript.sh

--- lpadmin_minus_p_printerscript.sh
+++ lpadmin_minus_p_printerscript.sh
@@ -1,6 +1,32 @@
 #!/bin/sh
+
+#begin printer example 1
 # install driver 1st
-#begin 19f printer
+MODEL="Konica-Minolta-C454e-PS"
+DISPLAY_NAME="PW WEST"
+LOCATION="PW WEST"
+PROTOCOL="lpd"
+DEVICE_URI="10.1.50.248"
+DRIVER="/Library/Printers/PPDs/Contents/Resources/KONICAMINOLTAC454e.gz"
+
+/usr/sbin/lpadmin -p "${MODEL}" \
+  -D "${DISPLAY_NAME}" \
+  -L "${LOCATION}" \
+  -E -v "${PROTOCOL}://${DEVICE_URI}" \
+  -P "${DRIVER}" \
+  -o printer-is-shared=false 
+
+  -o tonersavemode=on \
+  -o duplex=duplexnotumble \
+  -o CNPdeUseJobAccount=True \
+  -o CNUseJobAccount=True \
+  -o CNAuthenticate=True
+  
+  
+
+
+#begin printer example 2
+# install driver 1st
 MODEL="Canon-iR-ADV-C250-350"
 DISPLAY_NAME="19f"
 LOCATION="19f"
@@ -21,12 +47,8 @@ DRIVER="/Library/Printers/PPDs/Contents/Resources/CNPZUIRAC250ZU.ppd.gz"
   -o CNAuthenticate=True
   
   
-  
+#begin printer example 3  
 # install driver 1st
-#begin 15f 
-#begin 15f printer
-#begin 15f printer
-#begin 15f printer
 
 MODEL="Canon-iR-ADV-C250-350-15f"
 DISPLAY_NAME="15f"
