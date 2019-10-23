6a3bfe82e56f2e1819fe50b41c9e65d2afdb2867
zackn9ne <zackn9ne@gmail.com>
2019-10-22 20:50:26 -0400

    Create dep_notify_user_maker.sh

---
+++ dep_notify_user_maker.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Set jamfbin
+JAMFBIN=$(/usr/bin/which jamf)
+
+# Get the current logged in user that we'll be modifying
+if [ ! -z "$3" ]; then
+	CURRENTUSER=$3
+else
+	CURRENTUSER=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+fi
+
+# Get location of default DEPnotify plist form JPS
+DNPLIST=/Users/"$CURRENTUSER"/Library/Preferences/menu.nomad.DEPNotifyUserInput.plist
+
+# Get username var
+USERNAME=$(/usr/libexec/plistbuddy $DNPLIST -c "print 'Computer Name'" | tr [A-Z] [a-z])
+
+# Set username var
+$JAMFBIN createAccount -username $USERNAME -realname $USERNAME -password Welcome2019 -admin
