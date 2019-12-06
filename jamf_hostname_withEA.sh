d9ceb7976b76fd622630be060aaa0409c5a8b892
zackn9ne <zackn9ne@gmail.com>
2019-12-06 16:10:03 -0500

    Update jamf_hostname_withEA.sh
    supply user input in Jamf Pro
    
    field 4 is location,
    field 5 is type of mac

--- jamf_hostname_withEA.sh
+++ jamf_hostname_withEA.sh
@@ -1,75 +1,39 @@
 #!/bin/bash
 
 #welcome to zackn9nes jamf hostname script
-jamfbinary='/usr/local/bin/jamf'
-jssURL="https://xxx.jamfcloud.com/JSSResource/"
-usernamePasswordHash='xxx'
-apiEndpointEA='computerextensionattributes'
-apiEndpointSerial='computers/serialnumber'
-Location=$4 #for jamf user input
-#auto get serial on device side
-SERIAL=$(ioreg -c IOPlatformExpertDevice -d 2 | awk -F\" '/IOPlatformSerialNumber/{print $(NF-1)}')
+jamfbinary=$(/usr/bin/which jamf)
+location=$4 #for jamf operator input
+model=$5 #for jamf operator input
 
-#test specific serial here
-#SERIAL='xxx'
-
-#you will need an EA at id 8 which culls the last user test them with this block
-#----
-#availableEAs=${jssURL}${apiEndpointEA}
-#echo "payload is" $availableEAs
-#curl -k ${availableEAs} -H "Accept: application/json" -H "Authorization: Basic ${usernamePasswordHash}"
-#---
-
-
-echo "now getting last user for" $SERIAL
-EAID=8
-echo parsing ${jssURL}${apiEndpointSerial}/${SERIAL}/subset/extension_attributes
-xml=$(curl -k ${jssURL}${apiEndpointSerial}/${SERIAL}/subset/extension_attributes -H "Accept: application/xml" -H "Authorization: Basic ${usernamePasswordHash}")
-echo "xml is>>>>>" $xml
-LastUser=$(echo $xml | xpath "//*[id=$EAID]/value/text()")
-echo "last user is" $LastUser
-
-#searialism
-DEVICEINFO=$(curl -k ${jssURL}${apiEndpointSerial}/${SERIAL}/subset/hardware -H "Accept: application/xml" -H "Authorization: Basic ${usernamePasswordHash}")
-MODEL=$(echo $DEVICEINFO | /usr/bin/awk -F'<model_identifier>|</model_identifier>' '{print $2}')
-echo $MODEL
-
+#whos logged in
+user=$( scutil <<< "show State:/Users/ConsoleUser" | awk -F': ' '/[[:space:]]+Name[[:space:]]:/ { if ( $2 != "loginwindow" ) { print $2 }}' )
+echo "detected location is" $location
+echo "detected user is:" $user
 
 #killswitch
-if [ -z "$LastUser" ]
+if [ -z "$user" ]
 then
-	echo "LastUser is broken exiting with error"
+	echo "user is null exiting with error"
     exit 1
-elif [ -z "$Location" ]
+elif [ $user = "root" ]
+then
+     echo "user is root user failing gracefully"
+     exit 1
+elif [ -z "$location" ]
 then
 	echo "Location is broken exiting with error"
     exit 1
+elif [ -z "$model" ]
+then
+	echo "Model is broken exiting with error"
+    exit 1
+
 else
     echo "proceeding"
 fi
 
 #back to the device
-if echo "$MODEL" | grep -q "MacBookAir"
-then
-	PREFIX="MBA"
-elif echo "$MODEL" | grep -q "MacBookPro"
-then
-	PREFIX="MBP"
-elif echo "$MODEL" | grep -q "Mac Mini"
-then
-	PREFIX="MM"
-elif echo "$MODEL" | grep -q "iMac"
-then
-	PREFIX="IM"
-elif echo "$MODEL" | grep -q "MacPro"
-then
-	PREFIX="MP"
-else
-	echo "No model identifier found."
-	PREFIX="uNK"
-fi
-
-hostname="${Location}-${PREFIX}-${LastUser}"
+hostname="${location}-${model}-${user}"
 hostname=${hostname:0:15}
 echo $hostname
 
