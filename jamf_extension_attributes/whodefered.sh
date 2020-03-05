88f877bdd21fbbe03ef797d6e6313b2c67ee3a24
zackn9ne <zackn9ne@gmail.com>
2020-03-05 14:05:28 -0500

    Update whodefered.sh
    Find out if there are deferrals pending

--- jamf_extension_attributes/whodefered.sh
+++ jamf_extension_attributes/whodefered.sh
@@ -3,7 +3,8 @@
 FILE="/Library/Application Support/JAMF/.userdelay.plist"
 if [ -f "$FILE" ]; then
    if grep -q date "$FILE"; then
-   echo "<result>Deferals Exist</result>"
+      echo "<result>Deferals Exist</result>"
+   fi
 else
    echo "<result>Nothing Defered</result>"
 fi
