7c6223240636f7ec9d1efbd8d920798f16de8299
zackn9ne <zackn9ne@gmail.com>
2020-04-23 11:07:31 -0400

    Update clear_fails.sh

--- depnotify_tools/clear_fails.sh
+++ depnotify_tools/clear_fails.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-sudo pkill "DEPNotify"
-sudo rm -rf /var/tmp/depnotify.log
-sudo rm -rf /var/tmp/dep*
-sudo rm -rf /var/tmp/com.depnotify.*
-rm -rf ~/Library/Preferences/menu.nomad*
+
+# Must be run in sudo
+  pkill -i Depnotify
+  
+# Removing config files in /var/tmp
+  rm /var/tmp/depnotify*
+
+# Removing bom files in /var/tmp
+  rm /var/tmp/com.depnotify.*
+
+# Removing plists in local user folder
+  CURRENT_USER=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+  rm /Users/"$CURRENT_USER"/Library/Preferences/menu.nomad.DEPNotify*
+
+# Restarting cfprefsd due to plist changes
+  killall cfprefsd
