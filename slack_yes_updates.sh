d7a63624a2929d94a371e2556cbbdabbbf0ef784
zackn9ne <zackn9ne@gmail.com>
2020-06-02 11:55:28 -0400

    Delete slack_yes_updates.sh

--- slack_yes_updates.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#variable for storing the current users name
-#this lets the update slack button show back up
-currentuser=`stat -f "%Su" /dev/console`
-
-#substituting as user stored in variable to modify plist
-su "$currentuser" -c "defaults write com.tinyspeck.slackmacgap SlackNoAutoUpdates -bool NO"
